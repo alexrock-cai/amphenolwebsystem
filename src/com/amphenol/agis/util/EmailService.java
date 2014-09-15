b3557f84c5b13d0ae04361e6947e14ffa04b13ee
rocky <cww.jerry@gmail.com>
2014-09-15 22:37:07 +0800

    增加测试方法

--- src/com/amphenol/agis/util/EmailService.java
+++ src/com/amphenol/agis/util/EmailService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.mail.MessagingException;
 
+import com.amphenol.agis.model.PkgModel;
 import com.amphenol.agis.model.ShipdataModel;
 import com.amphenol.agis.util.Eml;
 
@@ -54,6 +55,43 @@ public class EmailService
 			}
 			
 			eml.setBody("这是一封测试邮件。 总共有："+list.size()+"块产品没有经过EEPROM，总共有"+notindts.size()+"块可能更换标签，因为在DTS中没有发现。清单如下：详情请访问：http://131.101.208.5 查看。谢谢！"+sb.toString());
+			eml.send();
+		}
+		catch (MessagingException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	public boolean sendNoEEPROMAndNoTagin(List<PkgModel> list,List<PkgModel> noTagin)
+	{
+		try
+		{
+			Eml eml=new Eml("smtp.126.com", "amphenolmaster@126.com", "amphenolmaster@126.com", "test123");
+			eml.addTo("rocky.cai@amphenol-tcs.com");
+			eml.addTo("chris.zhang@amphenol-tcs.com");
+			eml.addTo("kevin.ding@amphenol-tcs.com");
+			eml.addTo("jackie.pan@amphenol-tcs.com");
+			eml.addTo("susan.fan@amphenol-tcs.com");
+			eml.setSubject("EEPROM 系统提示信息");
+			
+			StringBuilder sb=new StringBuilder();
+			sb.append("<p>没有经过EEPROM的清单</p>");
+			for(PkgModel ship : list)
+			{
+				sb.append("<p>customer_sn:"+ship.getStr("customer_sn")+"*******WO#:"+ship.getStr("wo")+"******PN#:"+ship.getStr("pn")+"</p>");
+			}
+			
+			sb.append("<p>如下是可能更换标签的产品</p>");
+			
+			for(PkgModel ship : noTagin)
+			{
+				sb.append("<p>customer_sn:"+ship.getStr("customer_sn")+"*******WO#:"+ship.getStr("wo")+"******PN#:"+ship.getStr("pn")+"</p>");
+			}
+			
+			eml.setBody("这是一封测试邮件。 总共有："+list.size()+"块产品没有经过EEPROM，总共有"+noTagin.size()+"块可能更换标签，因为在DTS中没有发现。清单如下：详情请访问：http://131.101.208.5 查看。谢谢！"+sb.toString());
 			eml.send();
 		}
 		catch (MessagingException e)
